fa9ce8412383f18c80574f9d95a249dbfb2821cb
Valente, Alexsander <alevtelles@gmail.com>
2024-07-30 18:14:23 -0300

    Add jazzmin

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "jazzmin",
+
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -40,6 +42,8 @@ INSTALLED_APPS = [
 
     "django_celery_results",
     "django_celery_beat",
+
+    "rest_framework",
     
     'stocks'
 ]
---
+++ stocks/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('stocks/', views.StockPriceView.as_view(), name='get_stock_price')
+]
