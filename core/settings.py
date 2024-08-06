375d228fe00de7f05afd368b8964e2782c088e6b
Valente, Alexsander <alevtelles@gmail.com>
2024-08-06 09:45:01 -0300

    add docker-compose and update settings/core

--- core/settings.py
+++ core/settings.py
@@ -131,6 +131,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CELERY_BROKER_URL = "pyamqp://guest@localhost//"
+CELERY_BROKER_URL = "pyamqp://guest@rabbitmq//"
+
 CELERY_TIMEZONE = "America/Sao_Paulo"
 CELERY_RESULT_BACKEND = "django-db"
